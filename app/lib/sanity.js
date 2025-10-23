b3b1eebb66ca2f3e6ae5ed75a8acaa5ae45f3bf6
rinormaliqi <rmaliqi75@gmail.com>
2025-10-23 18:47:14 +0200

    2.1.1

--- app/lib/sanity.js
+++ app/lib/sanity.js
@@ -3,11 +3,13 @@ import { createClient } from 'next-sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: 'yw6b68x6', // e.g. "abcd1234"
+  projectId: 'yw6b68x6',
   dataset: 'production',
   apiVersion: '2025-01-01',
-  useCdn: true,
+  useCdn: false, // ⚠️ turn off CDN for fresh content
 })
+export const revalidate = 0 // or 'force-dynamic' in Next 14+
+
 
 const builder = imageUrlBuilder(client)
 export const urlFor = (source) => builder.image(source)
